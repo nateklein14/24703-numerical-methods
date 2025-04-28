8e3fa8b960efa506dbfee5a08c73afebcdd82c7c
Nate Klein <113559390+nateklein14@users.noreply.github.com>
2025-04-28 13:52:37 -0400

    Added iteration tracking and removed unused imports

--- project/are.py
+++ project/are.py
@@ -2,9 +2,7 @@ import numpy as np
 from scipy.linalg import solve_continuous_are
 from scipy.linalg import solve_continuous_lyapunov
 from scipy.io import loadmat
-from scipy import signal
 from time import time
-import random
 import matplotlib.pyplot as plt
 
 # The Algebraic Riccati equation
@@ -19,6 +17,7 @@ def exact_line_search(A, G, Q, tol, X0=None):
         X = np.zeros(A.shape)
 
     # The algorithm described in Benner
+    i = 0
     t = 1
     R = ARE(A, G, Q, X)
     while np.linalg.norm(R, 'fro') > tol:
@@ -27,7 +26,8 @@ def exact_line_search(A, G, Q, tol, X0=None):
         X_new = X + t*N
         X = X_new
         R = ARE(A, G, Q, X)
-    return X
+        i += 1
+    return X, i
 
 # Newton-Raphson root finding for minimizing the error estimate
 def find_t(G, N, R):
@@ -57,10 +57,12 @@ def newton_kleinman(A, G, Q, tol, X0=None):
     else:    
         X = np.zeros(A.shape)
 
+    i = 0
     while np.linalg.norm(ARE(A, G, Q, X), 'fro') > tol:
         X_new = solve_continuous_lyapunov((A+G@X).T, X@G@X-Q)
         X = X_new
-    return X
+        i += 1
+    return X, i
 
 # Run each solver on systems of order 2^n for n = 1, 2, ..., n_max
 # Systems must be pre-saved as "sys{n}.mat"
@@ -68,12 +70,15 @@ def newton_kleinman(A, G, Q, tol, X0=None):
 def run_tests(n_max):
     nk_times = []
     nk_norms = []
+    nk_iters = []
     els_times = []
     els_norms = []
+    els_iters = []
     sp_times = []
     sp_norms = []
     stacked_times = []
     stacked_norms = []
+    stacked_iters = []
 
     tol = 1e-4
 
@@ -92,23 +97,26 @@ def run_tests(n_max):
         sp_norms.append(np.linalg.norm(R, 'fro'))
         
         t = time()
-        X = newton_kleinman(A, G, Q, tol)
+        X, i = newton_kleinman(A, G, Q, tol)
         nk_times.append(1000*(time() - t))
         R = ARE(A, G, Q, X)
         nk_norms.append(np.linalg.norm(R, 'fro'))
+        nk_iters.append(i)
         
         t = time()
-        X = exact_line_search(A, G, Q, tol)
+        X, i = exact_line_search(A, G, Q, tol)
         els_times.append(1000*(time() - t))
         R = ARE(A, G, Q, X)
         els_norms.append(np.linalg.norm(R, 'fro'))
+        els_iters.append(i)
 
         t = time()
-        X = newton_kleinman(A, G, Q, 10*tol)
-        X = exact_line_search(A, G, Q, tol, X0=X)
+        X, i_nk = newton_kleinman(A, G, Q, 10*tol)
+        X, i_els = exact_line_search(A, G, Q, tol, X0=X)
         stacked_times.append(1000*(time() - t))
         R = ARE(A, G, Q, X)
         stacked_norms.append(np.linalg.norm(R, 'fro'))
+        stacked_iters.append(i_nk + i_els)
 
         print(order)
 
@@ -120,6 +128,9 @@ def run_tests(n_max):
     np.save("nk_norms.npy", nk_norms)
     np.save("els_norms.npy", els_norms)
     np.save("stacked_norms.npy", stacked_norms)
+    np.save("nk_iters.npy", nk_iters)
+    np.save("els_iters.npy", els_iters)
+    np.save("stacked_iters.npy", stacked_iters)
 
 # Load results from the .npy files
 def load_results():
@@ -131,41 +142,52 @@ def load_results():
     nk_norms = np.load("nk_norms.npy")
     els_norms = np.load("els_norms.npy")
     stacked_norms = np.load("stacked_norms.npy")
-    return sp_times, nk_times, els_times, stacked_times, sp_norms, nk_norms, els_norms, stacked_norms
+    nk_iters = np.load("nk_iters.npy")
+    els_iters = np.load("els_iters.npy")
+    stacked_iters = np.load("stacked_iters.npy")
+    return sp_times, nk_times, els_times, stacked_times, sp_norms, nk_norms, els_norms, stacked_norms, nk_iters, els_iters, stacked_iters
 
 # Plot the results of the tests
-def plot_results(sp_times, nk_times, els_times, stacked_times, sp_norms, nk_norms, els_norms, stacked_norms):
+def plot_results(sp_times, nk_times, els_times, stacked_times, sp_norms, nk_norms, els_norms, stacked_norms, nk_iters, els_iters, stacked_iters):
 
     plt.xlabel("$log_2(Model Order)$")
     plt.ylabel("Runtime (ms)")
-    plt.title("SciPy Runtime")
-    plt.plot(range(1,len(sp_times)+1), sp_times)
-    plt.show()
+    plt.title("Runtimes")
+    plt.plot(range(1,len(sp_times)+1), sp_times, linewidth=3, color='m')
+    # plt.show()
 
     plt.xlabel("$log_2(Model Order)$")
     plt.ylabel("Runtime (ms)")
-    plt.title("Newton-Kleinman Runtime")
-    plt.plot(range(1,len(nk_times)+1), nk_times)
+    # plt.title("Newton-Kleinman Runtime")
+    plt.plot(range(1,len(nk_times)+1), nk_times, linewidth=3, color='r')
+    # plt.show()
+
+    plt.xlabel("$log_2(Model Order)$")
+    plt.ylabel("Runtime (ms)")
+    # plt.title("Stacked Methods Runtime")
+    plt.plot(range(1,len(stacked_times)+1), stacked_times, linewidth=3, color='g')
+    plt.legend(["SciPy", "Newton-Kleinman", "Stacked Methods"])
     plt.show()
 
     plt.xlabel("$log_2(Model Order)$")
     plt.ylabel("Runtime (ms)")
     plt.title("Exact Line Search Runtime")
-    plt.plot(range(1,len(els_times)+1), els_times)
+    plt.plot(range(1,len(els_times)+1), els_times, linewidth=3, color='b')
     plt.show()
 
     plt.xlabel("$log_2(Model Order)$")
-    plt.ylabel("Runtime (ms)")
-    plt.title("Stacked Methods Runtime")
-    plt.plot(range(1,len(stacked_times)+1), stacked_times)
+    plt.ylabel("Iterations")
+    plt.title("Iterations by Method")
+    plt.plot(range(1,len(nk_iters)+1), nk_iters, linewidth=3, color='r')
+    plt.plot(range(1,len(els_iters)+1), els_iters, linewidth=3, color='b')
+    plt.plot(range(1,len(stacked_iters)+1), stacked_iters, linewidth=3, linestyle='--', color='g')
+    plt.legend(["Newton-Kleinman", "Exact Line Search", "Stacked Methods"])
     plt.show()
 
-    # plt.legend(["SciPy", "Newton-Kleinman", "Exact Line Search"])
-
-    plt.plot(range(1,len(sp_norms)+1), sp_norms)
-    plt.plot(range(1,len(nk_norms)+1), nk_norms)
-    plt.plot(range(1,len(els_norms)+1), els_norms)
-    plt.plot(range(1,len(stacked_norms)+1), stacked_norms)
+    plt.plot(range(1,len(sp_norms)+1), sp_norms, linewidth=3, color='m')
+    plt.plot(range(1,len(nk_norms)+1), nk_norms, linewidth=3, color='r')
+    plt.plot(range(1,len(els_norms)+1), els_norms, linewidth=3, color='b')
+    plt.plot(range(1,len(stacked_norms)+1), stacked_norms, linewidth=3, linestyle='--', color='g')
     plt.xlabel("$log_2(Model Order)$")
     plt.ylabel("$||R(X_j)||_F$")
     plt.title("Solution Error")
@@ -173,5 +195,5 @@ def plot_results(sp_times, nk_times, els_times, stacked_times, sp_norms, nk_norm
     plt.show()
 
 # run_tests(n_max=7) # Comment this out if just plotting previously run tests
-sp_times, nk_times, els_times, stacked_times, sp_norms, nk_norms, els_norms, stacked_norms = load_results()
-plot_results(sp_times, nk_times, els_times, stacked_times, sp_norms, nk_norms, els_norms, stacked_norms)
+sp_times, nk_times, els_times, stacked_times, sp_norms, nk_norms, els_norms, stacked_norms, nk_iters, els_iters, stacked_iters = load_results()
+plot_results(sp_times, nk_times, els_times, stacked_times, sp_norms, nk_norms, els_norms, stacked_norms, nk_iters, els_iters, stacked_iters)
